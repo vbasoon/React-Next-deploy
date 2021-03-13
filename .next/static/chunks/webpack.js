d3e286244fa6572425d1592c05e9f37da54df8df
Басун Володимир <vbasoon@gmail.com>
2021-03-13 13:09:43 +0200

    ServerSideProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "3ccb3f28affaf7e5e63f";
+/******/ 	var hotCurrentHash = "f5761b8aa2cc4d89f277";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
