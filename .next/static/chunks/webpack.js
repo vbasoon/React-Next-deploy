49aabc9e2f29fed4903994e1f8c24111f2f5de5a
Басун Володимир <vbasoon@gmail.com>
2021-03-13 11:28:30 +0200

    getStaticProps

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "20ceee6d1ebd905d7d90";
+/******/ 	var hotCurrentHash = "66efa490e674570ceb8a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/users.js
+++ pages/users.js
@@ -29,3 +29,9 @@ const Users = () => {
 };
 
 export default Users;
+
+export async function getStaticProps(context) {
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
