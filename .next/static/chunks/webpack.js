bb02548e417ac6f542f93138e47787892b93fd6d
Басун Володимир <vbasoon@gmail.com>
2021-03-13 11:34:10 +0200

    server rendering users

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "66a9bab8076c1abb3b62";
+/******/ 	var hotCurrentHash = "ead32908ec510d2341ac";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/users.js
+++ pages/users.js
@@ -1,4 +1,3 @@
-import { useState, useEffect } from "react";
 import Link from "next/link";
 
 const Users = ({ users }) => {
