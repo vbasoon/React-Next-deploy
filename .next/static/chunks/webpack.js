ee3221527cf7954d4aa27008eb2308df775dfd42
Басун Володимир <vbasoon@gmail.com>
2021-03-13 12:58:53 +0200

    user name

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ead32908ec510d2341ac";
+/******/ 	var hotCurrentHash = "3ccb3f28affaf7e5e63f";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,12 +1,23 @@
 import { useRouter } from "next/router";
 import styles from "../../styles/user.module.scss";
 
-export default function User() {
+export default function User({ user }) {
   const { query } = useRouter();
 
   return (
     <div className={styles.user}>
       <h1>User ID {query.id}</h1>
+      <div>User name - {user.name}</div>
     </div>
   );
+}
+
+export async function getServerSideProps({ params }) {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${params.id}`
+  );
+  const user = await response.json();
+  return {
+    props: { user }, // will be passed to the page component as props
+  };
 }
